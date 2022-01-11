9ccec08efa82ff0f154167d9f685c1e15db5f76d
heavensbee001 <heavensbee001@gmail.com>
2022-01-11 21:11:45 +0100

    Section 2  - Add attack mechanics and events

--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,16 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy(
-      ["Saitama", "Jinx", "Tanjiro"],       // Names
-      ["https://i.imgur.com/pKd5Sdk.png", // Images
+    const gameContract = await gameContractFactory.deploy(                        
+      ["Leo", "Aang", "Pikachu"],       
+      ["https://i.imgur.com/pKd5Sdk.png", 
       "https://i.imgur.com/xVu4vFL.png", 
-      "https://i.imgur.com/WMB6g9u.png"],
-      [100, 200, 300],                    // HP values
-      [100, 50, 25]                       // Attack damage values
+      "https://i.imgur.com/u7T87A6.png"],
+      [100, 200, 300],                    
+      [100, 50, 25],
+      "Elon Musk", // Boss name
+      "https://i.imgur.com/AksR0tt.png", // Boss image
+      10000, // Boss hp
+      50 // Boss attack damage
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
@@ -20,6 +24,12 @@ const main = async () => {
     // Get the value of the NFT's URI.
     let returnedTokenUri = await gameContract.tokenURI(1);
     console.log("Token URI:", returnedTokenUri);
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+    
+    txn = await gameContract.attackBoss();
+    await txn.wait();
   };
   
   const runMain = async () => {
